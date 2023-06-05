73325d849e2c55c4d95b5d284a363e6253ce7068
Aria14cs <y.condo16@gmail.com>
2023-06-04 21:25:16 -0500

    Creacion de Componentes y Home

---
+++ my-app-skills/src/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './home.css'
+
+function HomeP() {
+  return (
+    <>
+      <h1 className="title">TALENTO UNIDO</h1>
+    </>
+  );
+};
+
+export {HomeP};
---
+++ my-app-skills/src/Log_in/LogIn.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './login.css'
+
+function LogInm(){
+ return(
+
+
+ );
+
+};
+
+export {LogInm}
